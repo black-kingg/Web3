02fff2bd6ac01fa4a7e32d594f2acf7b2bf96765
Mubarak <mudet04@gmail.com>
2023-10-09 19:10:26 +0100

    innit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -30,7 +30,7 @@ export const Nav = (props) => {
 							</Link>
 						))}
 						<button
-							onClicK
+							onClick
 							className={`bg-gradient-to-r from-[#115f59] to-[#068f83] text-white ml-20 py-2 px-5 rounded-sm text-base`}
 						>
 							Connect
